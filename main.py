8b157ba497cbdab66d250832b81b04fe592d1ca0
Pravallikavyas <30778153+Pravallikavyas@users.noreply.github.com>
2022-11-30 22:30:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+#Importing pymongo package to connect python and mongodb
 import pymongo
+#Creating a variable client to assign MongoClient instance
 client=pymongo.MongoClient()
+#Printing the list of databases in mongodb
 print(client.list_database_names( ))
+#Creating a variable db to assign student database
 db=client.students
+#Creating a variable to store all the documents in a collection.students.json is imported into exam collection in mongodb
 exam_data=db.exam
+#Prints collections present in students database
 print(db.list_collection_names())
+#Checking whether 200 documents in students.json is imported or not
 print(db.exam.count_documents({ }))
-#cursor=db.exam.find({ })
-#for doc in cursor:
- #   print(doc)
-#print(db.exam.find_one({"name":"aimee Zank"}))
-print("1>max")
+#A query to print the students who scored maximum marks in all 
+print("1>Students who scored maximum marks in all")
 max_marks=exam_data.aggregate([
     {"$unwind":"$scores"},
     {"$group":
@@ -25,7 +29,8 @@ max_marks=exam_data.aggregate([
 ])
 for doc in max_marks:
     print(doc)
-print("2>avg and pass marks")
+#A query to print the students who scored below average in exam and pass marks is 40%
+print("2>Students who scored below average in exam and pass marks is 40%")
 avg_marks=exam_data.aggregate([
     {"$unwind": "$scores"},
     {"$match": {'scores.type': 'exam', "scores.score": {"$gt": 40, "$lt": 60}}
@@ -33,7 +38,8 @@ avg_marks=exam_data.aggregate([
 ])
 for i in avg_marks:
     print(i)
-print("3>pass/fail")
+#A query to find out pass/fail students
+print("3>Studens who are pass/fail")
 x =exam_data.aggregate(
 [ {"$set":
    {"scores":
@@ -59,8 +65,8 @@ x =exam_data.aggregate(
   ])
 for i in x:
     print(i)
-
-print("5")
+#A query to store documents of students who scored below average and above 40% in collection below average
+print("5>Students who scored below average and above 40% ")
 passandavg = db.belowaverage
 p =exam_data.aggregate(
 [{"$match":
